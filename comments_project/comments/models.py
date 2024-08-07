39152f8505f0b060ebc7da8062b58614248d2e7e
Yurii Koidan <yurii.koidan@gubadoo.app>
2024-08-07 14:18:57 +0300

    Added dependency file, added DB connection, created models

--- comments_project/comments/models.py
+++ comments_project/comments/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator, URLValidator
+from django.utils import timezone
+from django.utils.html import strip_tags
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=150,
+        validators=[RegexValidator(r'^[\w.-]+$', 'Введите только латинские буквы, цифры, точки и дефисы')],
+        unique=True
+    )
+    email = models.EmailField(validators=[EmailValidator()])
+    home_page = models.URLField(validators=[URLValidator()], blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    def clean(self):
+        self.text = strip_tags(self.text)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} at {self.created_at}"
+
+    def like(self):
+        self.likes += 1
+        self.save()
+
+    class Meta:
+        ordering = ['-created_at']
